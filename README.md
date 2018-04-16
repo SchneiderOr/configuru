51a3980d9571cec738c71f10cb0f9181d40c5776
Or Schneider <contact@SchneiderOr.com>
2018-04-16 23:32:16 +0300

    wips

---
+++ README.md
@@ -0,0 +1,17 @@
+# Configuru
+
+A small remote configuration library written for browser and node
+
+## Getting started
+
+1. Clone this project `git clone git@github.com:SchneiderOr/configuru.git`
+2. Run `yarn install` or `npm install` to get the project's dependencies
+2. Run `yarn mock-server or `npm run mock-server` to get the project's mock db server up and runing
+2. In another terminal, run node example/node.js to see an actual example
+
+## Scripts
+
+* `yarn build` or `npm run build` - produces production version of your library for both node and web under the `dist` folder
+* `yarn dev` or `npm run dev` - produces development version of your library and runs a watcher
+* `yarn test` or `npm run test`- runs the tests
+* `yarn test:watch` or `npm run test:watch` - same as above but in a watch mode
--- example/node.js
+++ example/node.js
@@ -1,9 +1,9 @@
-const Configuru = require('../dist/configuru.js-commonjs2')
+const Configuru = require('../dist/configuru-node')
 // We need to test our library so that we we will know it works and prevent regression of
 // changes in the future. How do you do it?
 const client = new Configuru({ apiKey: 'orKey1234' })
 
-client.on('fetching', () => console.log('fetching'))
+// client.on('fetching', () => console.log('fetching'))
 
 // We can check if the config is loaded from the server by accessing .isLoaded
 console.log('isLoaded:', client.isLoaded)
@@ -12,18 +12,18 @@ console.log('isLoaded:', client.isLoaded)
 client.on('loaded', config => {
 	console.log('initial config from server', JSON.stringify(config))
 	// Get a value by single key
-	console.log(client.get('key1'))
+	console.info('Get the value for key1', client.get('key1'))
 	// And default value also can be specified if key dosent exist or empty
-	console.log(client.get('key2', false))
+	console.info('Get the value for key12345 (that is not yet exist)', client.get('key2', false))
 	// Get a value by path
-	console.log(client.get(['key4', 'nested']))
+	console.info('Get the value for the path key4.nested:', client.get(['key4', 'nested']))
 })
 
 client.on('error', console.error)
 
-// get is astnc so we need to wait for loading before accessing it
+// get is async so we need to wait for the initial before accessing it
 setTimeout(() => {
-	console.log(client.get('key1', false))
+	console.log(client.get('key1'))
 }, 3000)
 
 process.stdin.on("data", () => { })
